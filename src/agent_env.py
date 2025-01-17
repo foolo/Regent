c931e32640b784c69b73e391244b508d83a9663a
Olof Andersson <olof.andersson@gmail.com>
2025-01-17 16:39:54 +0100

    M src/agent_env.py

--- src/agent_env.py
+++ src/agent_env.py
@@ -1,3 +1,4 @@
+from dataclasses import dataclass, field
 from datetime import datetime, timezone
 import os
 from praw import Reddit  # type: ignore
@@ -6,22 +7,24 @@ from src.pydantic_models.agent_state import AgentState
 from src.pydantic_models.agent_config import AgentConfig
 
 
+@dataclass
 class AgentEnv:
-	def __init__(self, state_filename: str, agent_config: AgentConfig, provider: BaseProvider, reddit: Reddit, confirm: bool, test_mode: bool, iteration_interval: int):
-		if os.path.exists(state_filename):
-			with open(state_filename) as f:
+	state_filename: str
+	agent_config: AgentConfig
+	provider: BaseProvider
+	reddit: Reddit
+	confirm: bool
+	test_mode: bool
+	iteration_interval: int
+	state: AgentState = field(init=False)
+
+	def __post_init__(self):
+		if os.path.exists(self.state_filename):
+			with open(self.state_filename) as f:
 				whole_file_as_string = f.read()
 				self.state = AgentState.model_validate_json(whole_file_as_string)
 		else:
 			self.state = AgentState(history=[], streamed_submissions=[], streamed_submissions_until_timestamp=datetime.fromtimestamp(0, timezone.utc))
-
-		self.state_filename = state_filename
-		self.agent_config = agent_config
-		self.provider = provider
-		self.reddit = reddit
-		self.confirm = confirm
-		self.test_mode = test_mode
-		self.iteration_interval = iteration_interval
 
 	def save_state(self):
 		with open(self.state_filename, 'w') as f:
