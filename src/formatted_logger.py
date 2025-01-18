4b5204237b37dd8ee9f3f00bf9756dc23f9e953c
Olof Andersson <olof.andersson@gmail.com>
2025-01-18 10:46:13 +0100

    M src/formatted_logger.py

--- src/formatted_logger.py
+++ src/formatted_logger.py
@@ -1,6 +1,7 @@
 from typing import Any
 from colorama import init as colorama_init
 from colorama import Fore, Style
+from src.log_config import logger
 
 
 class FormattedLogger:
@@ -9,13 +10,17 @@ class FormattedLogger:
 
 	def code(self, code: Any):
 		print(Fore.BLUE + str(code) + Style.RESET_ALL)
+		logger.debug(code)
 
 	def text(self, text: Any):
 		print(str(text))
+		logger.debug(text)
 
 	def header(self, level: int, text: Any):
 		print("")
-		print(Fore.GREEN + f"{'#' * level} {str(text)}" + Style.RESET_ALL)
+		header = f"{'#' * level} {str(text)}"
+		print(Fore.GREEN + header + Style.RESET_ALL)
+		logger.debug(header)
 
 
 fmtlog = FormattedLogger()
