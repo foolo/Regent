0337c27bded1a4107f0779f66c2e7317be69c956
Olof Andersson <olof.andersson@gmail.com>
2025-01-23 14:32:10 +0100

    M src/providers/openai_provider.py

--- src/providers/openai_provider.py
+++ src/providers/openai_provider.py
@@ -6,15 +6,15 @@ from src.pydantic_models.openai_config import OpenAIConfig
 
 class OpenAIProvider(BaseProvider):
 	def __init__(self, config: OpenAIConfig):
-		self._client = OpenAI(api_key=config.api_key)
-		self._model = config.model_id
+		self.client = OpenAI(api_key=config.api_key)
+		self.model = config.model_id
 
 	def get_action(self, system_prompt: str) -> Action | None:
 		messages: list[Any] = []
 		messages.append({"role": 'system', "content": system_prompt})
 
-		completion = self._client.beta.chat.completions.parse(
-		    model=self._model,
+		completion = self.client.beta.chat.completions.parse(
+		    model=self.model,
 		    messages=messages,
 		    response_format=Action,
 		)
